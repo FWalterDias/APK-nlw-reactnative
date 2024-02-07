f890398b95a4c238af625090af80b530980105d2
Francisco Walter <franciscowalterd@gmail.com>
2024-02-07 09:11:50 -0300

    adicionando header

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/header";
+import { View } from "react-native";
+
+
+export default function Home() {
+    return (
+        <View className="flex-1 pt-8">
+            <Header title="Faça seu pedido" />
+        </View>
+    )
+}
