a7e1031a548f3540e8e932fc7292ca594d5de5ab
Francisco Walter <franciscowalterd@gmail.com>
2024-02-07 12:04:39 -0300

    finalizando tela de produto/id

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,30 @@
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { View, FlatList } from "react-native";
-import { CATEGORIES } from "@/utils/data/products"
-import { useState } from "react";
+import { Product } from "@/components/products";
+import { CATEGORIES, MENU } from "@/utils/data/products";
+import { Link } from "expo-router";
+import { useState, useRef } from "react";
+import { FlatList, View, SectionList, Text } from "react-native";
 
 export default function Home() {
 
     const [category, setCategory] = useState<string>("");
 
+    const sectionListRef = useRef<SectionList>(null);
+
     function handleCategorySelect(selectedCategory: string) {
         setCategory(selectedCategory)
+
+        const sectionIndex = CATEGORIES.findIndex(category => category === selectedCategory);
+
+        if (sectionListRef.current) {
+            sectionListRef.current.scrollToLocation({
+                animated: true,
+                sectionIndex,
+                itemIndex: 0
+            })
+        }
+
     }
 
     return (
@@ -32,6 +47,31 @@ export default function Home() {
                 className="max-h-10 mt-5"
                 contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
                 showsHorizontalScrollIndicator={false}
+            />
+
+            <SectionList
+                ref={sectionListRef}
+                sections={MENU}
+                keyExtractor={(item) => item.id}
+                stickySectionHeadersEnabled={false}
+                renderItem={
+                    ({ item }) =>
+                    (
+                        <Link href={`/product/${item.id}`} asChild>
+                            <Product data={item} />
+                        </Link>
+                    )
+                }
+                renderSectionHeader={
+                    ({ section: { title } }) => (
+                        <Text className="text-xl text-white font-heading mt-8 mb-3">
+                            {title}
+                        </Text>
+                    )
+                }
+                className="flex-1 p-5"
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
             />
         </View>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/app/*.{js,jsx,ts,tsx}", "./src/components/*.{js,jsx,ts,tsx}"],
+  content: ["./src/app/**/*.{js,jsx,ts,tsx}", "./src/components/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
+      fontFamily: {
         heading: "Inter_600SemiBold",
         subtitle: "Inter_500Medium",
         body: "Inter_400Regular",
