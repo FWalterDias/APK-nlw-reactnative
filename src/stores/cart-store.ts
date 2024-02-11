5d83b2139f2faf00dd3726c5a6ff4bf5aeda13c9
Francisco Walter <franciscowalterd@gmail.com>
2024-02-11 14:19:01 -0300

    adicionando a funcionalidade de salvar os dados no storeage do dispositivo

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,8 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
 import * as cartInMemory from "./helpers/cart-in-memory";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 export type ProductCartProps = ProductProps & {
     quantity: number
@@ -12,14 +14,17 @@ type StateProps = {
     remove: (productId: string) => void
 }
 
-export const useCartStore = create<StateProps>((set) => ({
+export const useCartStore = create(persist<StateProps>((set) => ({
     products: [],
-    add: (product: ProductProps) => 
-    set((state) => ({
-        products: cartInMemory.add(state.products, product)
-    })),
+    add: (product: ProductProps) =>
+        set((state) => ({
+            products: cartInMemory.add(state.products, product)
+        })),
     remove: (productId: string) =>
-    set((state) => ({
-        products: cartInMemory.remove(state.products, productId)
-    }))
+        set((state) => ({
+            products: cartInMemory.remove(state.products, productId)
+        }))
+}), {
+    name: "nlw-wxpert:cart",
+    storage: createJSONStorage(() => AsyncStorage)
 }));
